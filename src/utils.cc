1f05556bff8030accbb79ad69a51bf53fea3360f
Yu-Ju Huang <gic4107@gmail.com>
2017-07-15 20:12:06 +0800

    Add DumpBinary in utils.

--- src/utils.cc
+++ src/utils.cc
@@ -12,3 +12,11 @@ bool IsFileExistent(const std::string& filename) {
 }
 
 }  // namespace OS
+
+void DumpBinary(const char* data, size_t size) {
+  for (int i = 0; i < size; i++) {
+    std::cout << std::setfill('0') << std::setw(2) << std::hex;
+    std::cout << (data[i] & 0xff);
+  }
+  std::cout << std::dec << std::endl;
+}
--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,7 @@
 #define _UTILS_H_
 
 #include <iostream>
+#include <iomanip>
 #include <sys/stat.h>
 
 #define DISALLOW_COPY_AND_ASSIGN(Class)     \
@@ -14,5 +15,7 @@ namespace OS {
 bool IsFileExistent(const std::string& filename);
 
 }  // namespace OS
+
+void DumpBinary(const char* data, size_t size);
 
 #endif  // _UTILS_H_
